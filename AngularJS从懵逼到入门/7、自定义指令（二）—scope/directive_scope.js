349109620135f754bebf57bb82280657a45c1cd5
wangqingqiang <qingqiang.wang@qq.com>
2016-06-17 18:01:50 +0800

    scope
    scope

--- AngularJS从懵逼到入门/7、自定义指令（二）—scope/directive_scope.js
+++ AngularJS从懵逼到入门/7、自定义指令（二）—scope/directive_scope.js
@@ -3,24 +3,47 @@
  */
 var myApp = angular.module('myApp', [])
     .controller('myAppCtr', ['$scope', function ($scope) {
-        $scope.name='这是控制器的name';
+        $scope.items=[{name:'Javascript'},{name:'C#'},{name:'Css'}];
+        $scope.title=121;
         $scope.hello=function(){
             console.log('hello world');
-        }
-        console.log('控制器');
+        };
+        console.log('controller')
     }])
     .directive('directiveScope', function () {
         return {
             scope: {
-                names:'&',
-                say:'&say'
+                say: '&say',
+                items: '&',
+                title: '@'
             },
             controller: function ($scope) {
-                console.log($scope.names());
+                $scope.title += "——测试一下独立作用域会影响同一个指令的多个调用吗1";
                 $scope.say()();
+                console.log('1');
+                console.log(typeof $scope.title)
+            },
+            restrict: 'EACM',
+            template: '<div>正在进行scope独立作用域测试1:<br/><ul><li ng-repeat="item in items()">{{item.name}}</li><li>{{title}}</li></ul></div>',
+            replace: true
+        }
+    })
+    .directive('directiveScope2', function () {
+        return {
+            scope: {
+                say:'&say',
+                items:'&',
+                title:'@'
+            },
+            controller: function ($scope) {
+                $scope.title+="——测试一下独立作用域会影响同一个指令的多个调用吗2";
+
+                $scope.say()();
+                console.log('2');
+
             },
             restrict:'EACM',
-            template:'<div>正在进行scope独立作用域测试</div>',
+            template:'<div>正在进行scope独立作用域测试2:<br/><ul><li ng-repeat="item in items()">{{item.name}}</li><li>{{title}}</li></ul></div>',
             replace:true
         }
     })
